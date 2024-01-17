0bcfbec2e41bd927ab453743df468ae06eed971e
Thanh <Thanh.ng.paris@gmail.com>
2024-01-17 18:25:31 +0100

    update content - form page

--- src/views/Create_User.vue
+++ src/views/Create_User.vue
@@ -1,5 +1,322 @@
 <template>
-  <div>
-    <h1>Create User Form Page</h1>
-  </div>
+  <main>
+    <HeaderView />
+    <form @submit="SubmitForm" class="mx-[25%] my-[5%]">
+      <div class="space-y-12">
+        <!-- Profile Block -->
+        <div class="border-b border-gray-900/10 pb-12">
+          <!-- Warning Message -->
+          <h2 class="text-base font-semibold leading-7 text-gray-900">
+            Profile
+          </h2>
+          <p class="mt-1 text-sm leading-6 text-gray-600">
+            Some of the information will be displayed publicly so be careful
+            what you share.
+          </p>
+
+          <!-- Profile Grid -->
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <!-- Username Section -->
+            <div id="UsernameSection" class="sm:col-span-3">
+              <label
+                for="username"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Username</label
+              >
+              <div class="mt-2">
+                <div
+                  class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                >
+                  <input
+                    v-model="User.username"
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Joseph"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <!-- About Section -->
+            <div id="AboutSection" class="col-span-full">
+              <label
+                for="about"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >About</label
+              >
+              <div class="mt-2">
+                <textarea
+                  v-model="User.about"
+                  id="about"
+                  name="about"
+                  rows="3"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <p class="mt-3 text-sm leading-6 text-gray-600">
+                Write a few sentences about yourself.
+              </p>
+            </div>
+
+            <!-- Photo Section -->
+            <div id="PhotoSection" class="col-span-full">
+              <label
+                for="photo"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Photo</label
+              >
+              <div class="mt-2 flex items-center gap-x-3">
+                <UserCircleIcon
+                  class="h-12 w-12 text-gray-300"
+                  aria-hidden="true"
+                />
+                <button
+                  type="button"
+                  class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Change
+                </button>
+              </div>
+            </div>
+
+            <!-- Cover Photo Section -->
+            <div class="col-span-full">
+              <label
+                for="cover-photo"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Cover photo</label
+              >
+              <div
+                class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+              >
+                <div class="text-center">
+                  <PhotoIcon
+                    class="mx-auto h-12 w-12 text-gray-300"
+                    aria-hidden="true"
+                  />
+                  <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                    <label
+                      for="file-upload"
+                      class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                    >
+                      <span>Upload a file</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        class="sr-only"
+                      />
+                    </label>
+                    <p class="pl-1">or drag and drop</p>
+                  </div>
+                  <p class="text-xs leading-5 text-gray-600">
+                    PNG, JPG, GIF up to 10MB
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Personnal Infos Block -->
+        <div class="border-b border-gray-900/10 pb-12">
+          <h2 class="text-base font-semibold leading-7 text-gray-900">
+            Personal Information
+          </h2>
+          <p class="mt-1 text-sm leading-6 text-gray-600">
+            Use a permanent address where you can receive mail.
+          </p>
+
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-3">
+              <label
+                for="first-name"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >First name</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.firstname"
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label
+                for="last-name"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Last name</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.lastname"
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-4">
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Email address</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.email"
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label
+                for="country"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Country</label
+              >
+              <div class="mt-2">
+                <select
+                  v-model="User.country"
+                  id="country"
+                  name="country"
+                  autocomplete="country-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>United States</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-span-full">
+              <label
+                for="street-address"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Street address</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.address"
+                  type="text"
+                  name="street-address"
+                  id="street-address"
+                  autocomplete="street-address"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2 sm:col-start-1">
+              <label
+                for="city"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >City</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.city"
+                  type="text"
+                  name="city"
+                  id="city"
+                  autocomplete="address-level2"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="region"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >State / Province</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.state"
+                  type="text"
+                  name="region"
+                  id="region"
+                  autocomplete="address-level1"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="postal-code"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >ZIP / Postal code</label
+              >
+              <div class="mt-2">
+                <input
+                  v-model="User.zip"
+                  type="text"
+                  name="postal-code"
+                  id="postal-code"
+                  autocomplete="postal-code"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Buttons -->
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          class="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
+  </main>
 </template>
+
+<script setup>
+import HeaderView from "./../components/Header.vue";
+import { PhotoIcon, UserCircleIcon } from "@heroicons/vue/24/solid";
+const User = {
+  username: "",
+  about: "",
+  firstname: "",
+  lastname: "",
+  email: "",
+  country: "",
+  address: "",
+  city: "",
+  state: "",
+  zip: "",
+};
+
+const SubmitForm = () => {
+  console.table(User);
+};
+</script>
