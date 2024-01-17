bdc40a54b79518604166e1a3a17e426ce5ec740b
Thanh <Thanh.ng.paris@gmail.com>
2024-01-17 20:17:50 +0100

    Home page - Intro content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,188 @@
 <template>
-  <HeaderView />
+  <div>
+    <HeaderView />
+    <div class="bg-white">
+      <!-- Header -->
+      <main>
+        <!-- Hero section -->
+        <div
+          class="relative isolate overflow-hidden bg-gray-900 pb-16 pt-14 sm:pb-20"
+        >
+          <img
+            src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2830&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply"
+            alt=""
+            class="absolute inset-0 -z-10 h-full w-full object-cover"
+          />
+          <div
+            class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            aria-hidden="true"
+          >
+            <div
+              class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              style="
+                clip-path: polygon(
+                  74.1% 44.1%,
+                  100% 61.6%,
+                  97.5% 26.9%,
+                  85.5% 0.1%,
+                  80.7% 2%,
+                  72.5% 32.5%,
+                  60.2% 62.4%,
+                  52.4% 68.1%,
+                  47.5% 58.3%,
+                  45.2% 34.5%,
+                  27.5% 76.7%,
+                  0.1% 64.9%,
+                  17.9% 100%,
+                  27.6% 76.8%,
+                  76.1% 97.7%,
+                  74.1% 44.1%
+                );
+              "
+            />
+          </div>
+          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl py-16 sm:py-40 lg:py-40">
+              <div class="text-center">
+                <h1
+                  class="text-4xl font-bold tracking-tight text-white sm:text-6xl"
+                >
+                  Full Stack CRUD web application
+                </h1>
+                <p class="mt-6 text-lg leading-8 text-gray-300">
+                  Update of my previous CRUD project in 2021 with more features,
+                  better design using the knowledge that i gain throughout the
+                  time. It will become a coding template for future references.
+                  Think about it like knowledge and mastery acquired that still
+                  has room to improve.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+            aria-hidden="true"
+          >
+            <div
+              class="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-20 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+              style="
+                clip-path: polygon(
+                  74.1% 44.1%,
+                  100% 61.6%,
+                  97.5% 26.9%,
+                  85.5% 0.1%,
+                  80.7% 2%,
+                  72.5% 32.5%,
+                  60.2% 62.4%,
+                  52.4% 68.1%,
+                  47.5% 58.3%,
+                  45.2% 34.5%,
+                  27.5% 76.7%,
+                  0.1% 64.9%,
+                  17.9% 100%,
+                  27.6% 76.8%,
+                  76.1% 97.7%,
+                  74.1% 44.1%
+                );
+              "
+            />
+          </div>
+        </div>
+
+        <!-- Feature section -->
+        <div class="mt-16 sm:mt-28 mb-16 lg:mb-24">
+          <div class="mx-auto max-w-7xl px-6 lg:px-8">
+            <div class="mx-auto max-w-2xl sm:text-center">
+              <h2 class="text-base font-semibold leading-7 text-indigo-600">
+                Everything you need
+              </h2>
+              <p
+                class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"
+              >
+                No server? No problem.
+              </p>
+              <p class="mt-6 text-lg leading-8 text-gray-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis.
+              </p>
+            </div>
+          </div>
+          <div class="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+            <dl
+              class="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16"
+            >
+              <div
+                v-for="feature in features"
+                :key="feature.name"
+                class="relative pl-9"
+              >
+                <dt class="inline font-semibold text-gray-900">
+                  <component
+                    :is="feature.icon"
+                    class="absolute left-1 top-1 h-5 w-5 text-indigo-600"
+                    aria-hidden="true"
+                  />
+                  {{ feature.name }}
+                </dt>
+                {{ " " }}
+                <dd class="inline">{{ feature.description }}</dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import HeaderView from "./../components/Header.vue";
+import {
+  ArrowPathIcon,
+  CloudArrowUpIcon,
+  Cog6ToothIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from "@heroicons/vue/20/solid";
+
+const features = [
+  {
+    name: "Push to deploy.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "SSL certificates.",
+    description:
+      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+    icon: LockClosedIcon,
+  },
+  {
+    name: "Simple queues.",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus.",
+    icon: ArrowPathIcon,
+  },
+  {
+    name: "Advanced security.",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Powerful API.",
+    description:
+      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+    icon: Cog6ToothIcon,
+  },
+  {
+    name: "Database backups.",
+    description:
+      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. ",
+    icon: ServerIcon,
+  },
+];
 </script>
