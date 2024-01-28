113c9c5ace2019492730c2d9d636159dfd9144b8
Thanh <Thanh.ng.paris@gmail.com>
2024-01-28 18:02:48 +0100

    Get User route

--- src/views/Read_User.vue
+++ src/views/Read_User.vue
@@ -2,9 +2,32 @@
   <div>
     <HeaderView />
     <h1>Read User Page</h1>
+    <main>
+      {{ Users }}
+    </main>
   </div>
 </template>
 
 <script setup>
 import HeaderView from "./../components/Header.vue";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+const Users = ref([]);
+
+const fetchUsers = () => {
+  axios
+    .get(`${import.meta.env.VITE_APP_BACKEND_URL}fs/api/get-user`)
+    .then((res) => {
+      console.log(res);
+      Users.value = res.data;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+onMounted(() => {
+  fetchUsers();
+});
 </script>
