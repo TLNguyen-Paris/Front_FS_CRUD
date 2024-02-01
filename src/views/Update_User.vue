9f22c55e456bf524f0edb44d0480396bd3e4d35e
Thanh <Thanh.ng.paris@gmail.com>
2024-02-01 01:51:55 +0100

    init template update user

--- src/views/Update_User.vue
+++ src/views/Update_User.vue
@@ -4,6 +4,281 @@
     <h1>Update User Page {{ this.$route.params.id }}</h1>
     <p>{{ route.params }}</p>
     <p>{{ user }}</p>
+    <form class="mx-[25%] my-[5%]">
+      <div class="space-y-12">
+        <!-- Profile Block -->
+        <div class="border-b border-gray-900/10 pb-12">
+          <!-- Warning Message -->
+          <h2 class="text-base font-semibold leading-7 text-gray-900">
+            Profile
+          </h2>
+          <p class="mt-1 text-sm leading-6 text-gray-600">
+            Some of the information will be displayed publicly so be careful
+            what you share.
+          </p>
+
+          <!-- Profile Grid -->
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <!-- Username Section -->
+            <div id="UsernameSection" class="sm:col-span-3">
+              <label
+                for="username"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Username</label
+              >
+              <div class="mt-2">
+                <div
+                  class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                >
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autocomplete="username"
+                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Joseph"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <!-- Profession Section -->
+            <div id="ProfessionSection" class="sm:col-span-3">
+              <label
+                for="profession"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Profession</label
+              >
+              <div class="mt-2">
+                <div
+                  class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                >
+                  <input
+                    type="text"
+                    name="profession"
+                    id="profession"
+                    autocomplete="profession"
+                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Joseph"
+                  />
+                </div>
+              </div>
+            </div>
+            <!-- About Section -->
+            <div id="AboutSection" class="col-span-full">
+              <label
+                for="about"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >About</label
+              >
+              <div class="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows="3"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+              <p class="mt-3 text-sm leading-6 text-gray-600">
+                Write a few sentences about yourself.
+              </p>
+            </div>
+
+            <!-- Photo Section -->
+            <div id="PhotoSection" class="col-span-full">
+              <label
+                for="photo"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Photo</label
+              >
+              <div class="mt-2 flex items-center gap-x-3">
+                <UserCircleIcon
+                  class="h-12 w-12 text-gray-300"
+                  aria-hidden="true"
+                />
+                <button
+                  type="button"
+                  class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                >
+                  Change
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Personnal Infos Block -->
+        <div class="border-b border-gray-900/10 pb-12">
+          <h2 class="text-base font-semibold leading-7 text-gray-900">
+            Personal Information
+          </h2>
+          <p class="mt-1 text-sm leading-6 text-gray-600">
+            Use a permanent address where you can receive mail.
+          </p>
+
+          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div class="sm:col-span-3">
+              <label
+                for="first-name"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >First name</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autocomplete="given-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label
+                for="last-name"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Last name</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autocomplete="family-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-4">
+              <label
+                for="email"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Email address</label
+              >
+              <div class="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autocomplete="email"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label
+                for="country"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Country</label
+              >
+              <div class="mt-2">
+                <select
+                  id="country"
+                  name="country"
+                  autocomplete="country-name"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  <option>United States</option>
+                  <option>France</option>
+                  <option>Swiss</option>
+                  <option>Belgium</option>
+                  <option>Canada</option>
+                  <option>Mexico</option>
+                  <option>Other</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-span-full">
+              <label
+                for="street-address"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >Street address</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="street-address"
+                  id="street-address"
+                  autocomplete="street-address"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2 sm:col-start-1">
+              <label
+                for="city"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >City</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  autocomplete="address-level2"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="region"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >State / Province</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="region"
+                  id="region"
+                  autocomplete="address-level1"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div class="sm:col-span-2">
+              <label
+                for="postal-code"
+                class="block text-sm font-medium leading-6 text-gray-900"
+                >ZIP / Postal code</label
+              >
+              <div class="mt-2">
+                <input
+                  type="text"
+                  name="postal-code"
+                  id="postal-code"
+                  autocomplete="postal-code"
+                  class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Buttons -->
+      <div class="mt-6 flex items-center justify-end gap-x-6">
+        <button
+          type="button"
+          class="text-sm font-semibold leading-6 text-gray-900"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
+    </form>
   </div>
 </template>
 
